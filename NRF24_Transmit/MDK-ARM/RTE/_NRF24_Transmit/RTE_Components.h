a760fbde0ae7f4b407968e528e30bc5ee51e3f24
Ngoc411 <lxn4112004@gmail.com>
2025-02-08 09:10:24 +0700

    Add files via upload

---
+++ NRF24_Transmit/MDK-ARM/RTE/_NRF24_Transmit/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'NRF24_Transmit' 
+ * Target:  'NRF24_Transmit' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
